d04ba6b824bfc24686aff3d7ffd8cf220ed07037
Lam Ngo <lamngo@Lams-MacBook-Pro.local>
2018-01-16 05:28:44 -0500

    update some security features and also update readme file

--- README.txt
+++ README.txt
@@ -26,6 +26,40 @@ final_grades/          : Stores final grade files for all students, only
 lab_grades/<user>/     : stores lab grades for all students, will contain student work eventually.
 
 
+
 TODO:
 
  - Find and fix security flaws?
+
+
+NOTE: To initialize users.txt file (set default password to be the same as user name), I write a separate script
+named initialize.py. It will change the passwords in the original text file to the hashed version and also include
+the salt value for each one.
+
+1. The first error is with using subprocess to pass on the contents of a single file. From reading the python
+documentation online, when we will sock.accept(), the server will block calls that hold the processes. Thus,
+when we use subprocess to cat a file, we would receive the error SSL3_GET_RECORD: wrong version number.
+
+Note: This is basically my thought process when I tried to decode, thus it can be wrong. 
+
+However, when I changed it to a simple routine: open the file, and then use makefile to write the content to the 
+client side, everything works fine.
+
+In addition, since we do not use subprocess anymore, we also remove the risk of shell injection.
+
+2. In addition, I included some additional guides to make the system more accessible to users. For example, I add a comment
+to say that an user does not have access to the grades on a certain port, to signify that they are using the wrong ports.
+Another example is to tell the users that they are entering invalid user names (not 8 characters, or not in the database).
+
+3. To improve security, when changing passwords, I make it so that the length of the passwords need to be greater than 3, to
+lower the chance of being brute forced.
+
+4. Make it so that only TLSv1.2 and later can make connections to the server to increase security. The server does not crash
+if an unsupported protocol is used. Tested using: openssl s_client -ssl3 -connect localhost:7353
+
+DESIGN PRINCIPLES REFLECTION
+
+
+
+
+
--- bin/server.py
+++ bin/server.py
@@ -48,11 +48,9 @@ addr = (host, port)
 buf_len = 4096
 
 
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.options &= ~ssl.OP_NO_SSLv3
-context.options &= ~ssl.OP_NO_SSLv2
-context.options &= ~ssl.OP_NO_TLSv1
-context.options &= ~ssl.OP_NO_TLSv1_1
+context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+context.options |= ssl.OP_NO_TLSv1
+context.options |= ssl.OP_NO_TLSv1_1
 context.load_cert_chain(certfile="../../certificate.pem", keyfile="../../key.pem")
 
 # Create TCP socket to listen on.
@@ -71,41 +69,33 @@ while True:
     # Wait for a new connection
     connection, client_address = sock.accept()
 
-    connstream = context.wrap_socket(connection, server_side=True)
-    #print('Accepted connection from', client_address)
-
-    f_in = connstream.makefile('r')
-    f_out = connstream.makefile('w')
-
-    # Request username.
-    f_out.write("Please enter username: ")
-    f_out.flush()
-    uname = f_in.readline().strip()
-
-    # Clean up username
-    if len(uname) < 8:
-        clean_uname = ""
-    else:
-        clean_uname = uname[0:8]
-
-    # Request password.
-    f_out.write("Please enter password: ")
-    f_out.flush()
-    pw = f_in.readline().strip()
     try:
+        connstream = context.wrap_socket(connection, server_side=True)
+        f_in = connstream.makefile('r')
+        f_out = connstream.makefile('w')
 
-        # Check for valid user/password pair.
-        if not clean_uname in passwords.keys():
-            f_out.write("Error: Invalid username\n")
-            f_out.flush()
+        # Request username.
+        f_out.write("Please enter username: ")
+        f_out.flush()
+        uname = f_in.readline().strip()
 
-        else:
-            hahsed_enter_password = hashlib.pbkdf2_hmac('sha256',pw.encode(),binascii.unhexlify(passwords[clean_uname][1].encode()),100000)
-            if not hmac.compare_digest(hahsed_enter_password, binascii.unhexlify(passwords[clean_uname][0].encode())):
+        # Clean up username
+        while len(uname) != 8 or not uname in passwords.keys():
+                f_out.write("Invalid. Please enter username again: ")
+                f_out.flush()
+                uname = f_in.readline().strip()
+
+        # Request password.
+        f_out.write("Please enter password: ")
+        f_out.flush()
+        pw = f_in.readline().strip()
+        try:
+            hahsed_enter_password = hashlib.pbkdf2_hmac('sha256',pw.encode(),binascii.unhexlify(passwords[uname][1].encode()),100000)
+            if not hmac.compare_digest(hahsed_enter_password, binascii.unhexlify(passwords[uname][0].encode())):
                 f_out.write("Error: Invalid username and password combination\n")
                 f_out.flush()
             else:
-                while pw == clean_uname:
+                while pw == uname or len(pw) < 3:
                     f_out.write("Please change your password.")
                     f_out.write("\n")
                     f_out.write("You will not be able to advance without doing so")
@@ -116,7 +106,7 @@ while True:
 
                 salt = os.urandom(16)
                 pw = hashlib.pbkdf2_hmac('sha256',pw.encode(),salt,100000)
-                passwords[clean_uname] = (binascii.hexlify(pw).decode(),binascii.hexlify(salt).decode())
+                passwords[uname] = (binascii.hexlify(pw).decode(),binascii.hexlify(salt).decode())
 
                 f_out.write("\nAccess granted. Here are your grades.\n")
                 try:
@@ -134,27 +124,30 @@ while True:
                 # # Wait for the process to complete.
                 # proc.wait()
 
-    # Terminate connection and clean up
-    finally:
-        connstream.close()
-        f_in.close()
-        f_out.close()
-        print("Closed connection from", client_address)
+        # Terminate connection and clean up
+        finally:
+            connstream.close()
+            f_in.close()
+            f_out.close()
+            print("Closed connection from", client_address)
 
-        try:
-            pw_file_r = open('../../users.txt','r')
-            pw_file_w = open('../../users_temp','w')
-        except:
-            print("Error: Unable to locate users.txt")
-            exit()
+            try:
+                pw_file_r = open('../../users.txt','r')
+                pw_file_w = open('../../users_temp','w')
+            except:
+                print("Error: Unable to locate users.txt")
+                exit()
 
-        lines = pw_file_r.readlines()
-        #<firstname> <username> <password> <port> <salt>
-        for line in lines:
-            toks = line.strip().split()
-            newstr = toks[0] + " " + toks[1] + " " + passwords[toks[1]][0] + " " + toks[3] + " " + passwords[toks[1]][1] + "\n"
-            pw_file_w.write(newstr)
+            lines = pw_file_r.readlines()
+            #<firstname> <username> <password> <port> <salt>
+            for line in lines:
+                toks = line.strip().split()
+                newstr = toks[0] + " " + toks[1] + " " + passwords[toks[1]][0] + " " + toks[3] + " " + passwords[toks[1]][1] + "\n"
+                pw_file_w.write(newstr)
 
-        pw_file_w.close()
-        pw_file_r.close()
-        os.rename('../../users.txt'.replace('.txt', '_temp'),'../../users.txt')
+            pw_file_w.close()
+            pw_file_r.close()
+            os.rename('../../users.txt'.replace('.txt', '_temp'),'../../users.txt')
+    #print('Accepted connection from', client_address)
+    except:
+        print ("Error: Unsupported protocol")
